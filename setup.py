aeae83cf7c38452e29cb536c2be6ca2ef60d9234
Ophélie MAUGER <ophelie.mauger@alterway.fr>
2018-06-27 14:40:00 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup
 
 setup(
-    name='Dependencies Injection',
+    name='dependencies-injection',
     url='https://github.com/omauger/dependencies_injection',
     author='Ophélie Mauger',
     packages=['dependencies_injection'],
     install_requires=[
         'inject==3.3.*'
     ],
-    version='0.1',
+    extras_require={
+        'ci': ['flake8', 'coverage'],
+    },
+    version='1.0',
     license='MIT',
     description='A module to implement easily dependencies injection in python project.',
 )
